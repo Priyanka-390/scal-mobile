46e50386b1dc571aba32d1a21238b0ad218cde6b
Priyanka-390 <psr82220@gmail.com>
2023-11-25 16:36:56 +0530

    navbar

--- main.js
+++ main.js
@@ -27,3 +27,9 @@ window.addEventListener('scroll', function () {
     backToTop.classList.add('hidden');
   }
 });
+function openNav() {
+  document.getElementById("navbar").classList.toggle("-left-full");
+  document.getElementById("navbar").classList.toggle("show");
+  document.body.classList.toggle("overflow-hidden");
+  document.getElementById("menubtn-icon").classList.toggle("cross")
+}
